644c67f09254b9e4b644a41c150435545d15da32
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-01-31 19:50:00 +0100

    ex00: output uppercase input

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -5,4 +5,16 @@ int main(int argc, char **argv)
 	(void) argv;
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			std::string str = argv[i];
+			for (std::basic_string<char>::size_type j = 0; j < str.size(); j++)
+			{
+				std::cout << (char)std::toupper(str[j]);
+			}
+		}
+		std::cout << std::endl;
+	}
 }
