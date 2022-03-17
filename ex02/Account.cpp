19995d96e37fe8ba3590bdfab4d9af3ff80feac8
GroteGnoom <8137208+GroteGnoom@users.noreply.github.com>
2022-03-17 11:28:07 +0100

    Add all account public functions

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -8,6 +8,22 @@ int Account::_totalAmount = 0;
 int Account::_totalNbDeposits = 0;
 int Account::_totalNbWithdrawals= 0;
 
+int Account::getNbAccounts() {
+	return _nbAccounts;
+}
+
+int Account::getTotalAmount() {
+	return _totalAmount;
+}
+
+int Account::getNbDeposits() {
+	return _totalNbDeposits;
+}
+
+int Account::getNbWithdrawals() {
+	return _totalNbWithdrawals;
+}
+
 void Account::_displayTimestamp() {
 	char buffer[100];
 	time_t t1;
